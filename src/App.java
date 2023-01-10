e8252f72f83823ffa510544eca98b4c360a1e02f
Lucas Lafèré <laferelucas@gmail.com>
2023-01-10 11:56:53 -0300

    added pyramid printing

--- src/App.java
+++ src/App.java
@@ -4,6 +4,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Informe o número a obter a pirâmide de números");
-        System.out.println("Hello, World!");
+        int numeroLinhasPiramide = scanner.nextInt();
+        for (int i = 1; i <= numeroLinhasPiramide; i++){
+            System.out.println(String.valueOf(i).repeat(i));
+        }
+        
     }
 }
