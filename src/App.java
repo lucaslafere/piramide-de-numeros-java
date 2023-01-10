7cfa6b14f83ca3f786b37a5843a1947f69ba88f5
Lucas Lafèré <laferelucas@gmail.com>
2023-01-10 11:54:30 -0300

    added scanner

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o número a obter a pirâmide de números");
         System.out.println("Hello, World!");
     }
 }
